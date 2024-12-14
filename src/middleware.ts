2c40378792142ce217a714beca720b1487324d1c
PaulSenon <paul.senon@gmail.com>
2024-12-14 17:21:17 +0900

    reproduction

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+    console.log('Hi From Custom Middleware');
+    
+    // Add the random value to locals so it's accessible in components
+    context.locals.myLocal = Math.random()*1000;
+    
+    // Get the response from the next handler
+    const response = await next();
+    
+    // Add our custom header
+    response.headers.set('X-Test-Value', Date.now().toLocaleString());
+    
+    return response;
+}) 
